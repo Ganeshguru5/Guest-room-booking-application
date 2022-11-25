d300820340a3f0325223a660f3bcafd117e89759
Ganeshguru <61460873+Ganeshguru5@users.noreply.github.com>
2022-11-25 20:10:08 +0530

    Update OwnedRoom.js

--- client/src/components/OwnedRoom.js
+++ client/src/components/OwnedRoom.js
@@ -4,7 +4,7 @@ export default function OwnedRoom(props) {
     return (
         <div className='singleHotel'>
             <div className=''>
-            <img src={'http://localhost:8080/'+props.URL} width='250px' alt='roomimg' className='roomimg'/>
+            <img src={'http://localhost:8080/'+props.URL} width='250px' height='250px' alt='roomimg' className='roomimg'/>
             </div>
             <span>{props.name}</span>
             <span style={{color:'#9f9f9f'}}>{props.address}</span>
