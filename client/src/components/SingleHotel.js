a54c4ea39414e1f6c3216e6e8291d01d2aadac60
Ganeshguru <61460873+Ganeshguru5@users.noreply.github.com>
2022-11-25 20:08:48 +0530

    Update SingleHotel.js

--- client/src/components/SingleHotel.js
+++ client/src/components/SingleHotel.js
@@ -5,7 +5,7 @@ export default function SingleHotel(props) {
   return (
     <div className='singleHotel'>
         <div className='img'>
-        <img src={'http://localhost:8080/'+props.URL} width='250px' alt='roomimg' className='roomimg'/>
+        <img src={'http://localhost:8080/'+props.URL} width='250px' height='250px' alt='roomimg' className='roomimg'/>
         </div>
         <span>{props.name}</span>
         <span style={{color:'#9f9f9f'}}>{props.address.slice(0,25)}</span>
